4748f5d8fc13e307569722482dab8c9586abb9a5
theprogrammergary <theprogrammergary@gmail.com>
2024-09-27 14:40:37 -0400

    set flask to use localhost

--- services/server/app.py
+++ services/server/app.py
@@ -54,3 +54,6 @@ async def ticker_data() -> tuple[str, int]:
     if request.json:
         r.publish(channel="ticker_updates", message=json.dumps(obj=request.json))
     return "Webhook received successfully", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
